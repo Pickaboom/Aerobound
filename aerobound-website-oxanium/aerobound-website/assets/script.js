879fd9e784860807c2e381e8bfbaecadc433e492
Pickaboom <levitully001@gmail.com>
2026-07-07 22:53:46 +1000

    Delete aerobound-website-oxanium/aerobound-website directory

--- aerobound-website-oxanium/aerobound-website/assets/script.js
+++
@@ -1,8 +0,0 @@
-
-document.querySelectorAll('.faq-q').forEach(button=>{
-  button.addEventListener('click',()=>{
-    const item = button.closest('.faq-item');
-    item.classList.toggle('open');
-  });
-});
-document.getElementById('year')?.append(new Date().getFullYear());
